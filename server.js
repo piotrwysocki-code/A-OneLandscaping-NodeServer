9605f312bec125146cc6848417a3717cb2fccd5b
piotrwysocki-code <wysockpi@sheridancollege.ca>
2022-05-16 13:29:23 -0400

    Hid some info via env file

--- server.js
+++ server.js
@@ -4,18 +4,19 @@ const express = require('express')
 const app = express()
 const mailer = require('nodemailer')
 const request = require('request')
-//const PORT = process.env.PORT || 4000
-require('dotenv').config(); 
-app.use(cors())
+require('dotenv').config()
+
+const corsOpts = {
+    origin: [process.env.ORIGIN1, process.env.ORIGIN2],
+    optionsSuccessStatus: 200 
+  }
+
+app.use(cors(corsOpts))
 
 let urlencodedParser = bodyParser.urlencoded({ extended: true })
-firebas
+
 app.use(bodyParser.urlencoded({entended: false}))
 app.use(bodyParser.json())
-
-//app.listen(PORT, ()=>{
-//   console.log(`listening on port ${PORT}`)
-//}) 
 
 app.get('/', (req, res)=> {
     res.send('<h1>A One Landscaping Mail Proxy</h1>');
@@ -60,23 +61,25 @@ app.post('/send', urlencodedParser, (req, res)=>{
 
     const output = `
         <h2>You have a new request for a quote!</h2>
+        <hr>
         <h3>Contact Details:</h3>
         <ul>
             <li>Name: ${req.body.name}</li>
-            <li>Phone No. ${req.body.phone}</li>
+            <li>Phone No.: ${req.body.phone}</li>
             <li>Email: ${req.body.email}</li>
             <li>Service Type: ${service}</li>
         </ul>
         <h4>Message</h4>
         <p>${req.body.message}</p>
-        <div style="display: flex; flex-direction: row; justify-content: center; align-items: flex-start; width: 100%; background-color: light-gray;">
-            <img style="width: 112; height: 81.3;" src="https://a1landscaping.s3.ca-central-1.amazonaws.com/img/a1-logo.png"/>
-            <h1 style="margin: 0;">Lanscaping Inc.</h1>
+        <hr>
+        <div>
+            <img style="width: 56; height: 40.65;" src="https://a1landscaping.s3.ca-central-1.amazonaws.com/img/a1-logo.png"/>
+            <h2 style="margin: 0;">Landscaping Inc.</h2>
         </div>
     `
 
     let trainsporter = mailer.createTransport({
-        host: 'email-smtp.us-east-1.amazonaws.com',
+        host: process.env.SMTP_HOST,
         port: 465,
         secure: true,
         auth: {
@@ -89,10 +92,10 @@ app.post('/send', urlencodedParser, (req, res)=>{
     })
 
     let options = {
-        from: `"A-One Landscaping Services" <services@a-onelandscaping.com`,
-        to: `adamwietczak@a-onelandscaping.com`,
+        from: `"A-One Landscaping Services" ${process.env.A_ONE_EMAIL}`,
+        to: process.env.A_ONE_EMAIL,
         subject: `[Quote Request] A-One - You've Got A Message!`,
-        text: `Hello, You\'ve recieved a quote request for ${service} services.`,
+        text: `Hello, You've recieved a quote request for ${service} services.`,
         html: output
     }
 
